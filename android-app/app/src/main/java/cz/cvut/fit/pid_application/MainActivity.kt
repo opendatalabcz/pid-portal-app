373a0f09a0d685f7f86283458d9d0617d27d2996
dosmagal <dosmagal@fit.cvut.cz>
2021-03-12 13:06:24 +0100

    android-app folder

---
+++ android-app/app/src/main/java/cz/cvut/fit/pid_application/MainActivity.kt
@@ -0,0 +1,11 @@
+package cz.cvut.fit.pid_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
